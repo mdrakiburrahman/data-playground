e9d5e2ca26f62ff0e48445b9e51ce88e4171a14c
Raki Rahman <mdrakiburrahman@gmail.com>
2025-06-30 20:55:11 -0400

    Checkpoint: Python, Rust, Docker

---
+++ contrib/bootstrap-dev-env.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+#
+#
+#       Sets up a dev env with all pre-reqs. This script is idempotent, it will
+#       only attempt to install dependencies, if not exists.   
+#
+# ---------------------------------------------------------------------------------------
+#
+
+set -e
+set -m
+
+export PYTHON_VERSION=3.11
+
+echo ""
+echo "┌────────────────────────────────────┐"
+echo "│ Checking for language dependencies │"
+echo "└────────────────────────────────────┘"
+echo ""
+
+if ! command -v python3 &> /dev/null || [[ "$(python3 --version 2>&1)" != *"$PYTHON_VERSION"* ]]; then
+    echo "Installing Python $PYTHON_VERSION..."
+    sudo apt update
+    sudo apt upgrade -y
+    sudo apt install -y python3 python3-pip python3-dev python3-venv build-essential
+fi
+
+if ! command -v rustc &> /dev/null; then
+    echo "Installing Rust..."
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+    source $HOME/.cargo/env
+fi
+
+echo ""
+echo "┌──────────────────────┐"
+echo "│ Installing CLI tools │"
+echo "└──────────────────────┘"
+echo ""
+
+if ! command -v docker &> /dev/null; then
+    echo "docker not found - installing..."
+    curl -sL https://get.docker.com | sudo bash
+fi
+sudo chmod 666 /var/run/docker.sock
+
+echo ""
+echo "┌───────────────────────────────┐"
+echo "│ Installing VS Code extensions │"
+echo "└───────────────────────────────┘"
+echo ""
+
+code --install-extension github.copilot
+code --install-extension ms-python.python
+code --install-extension ms-python.vscode-pylance
+code --install-extension vadimcn.vscode-lldb
+
+echo ""
+echo "┌──────────┐"
+echo "│ Versions │"
+echo "└──────────┘"
+echo ""
+
+echo "Docker: $(docker --version)"
